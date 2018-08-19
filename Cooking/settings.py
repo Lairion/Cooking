5aafff5152c75367208bc3eca1581719d8c74846
manul <nunyes_m_a@ukr.net>
2018-08-19 15:59:03 +0300

    Change settings dir

--- Cooking/settings.py
+++ Cooking/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,3 +134,5 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(os.path.dirname(PROJECT_ROOT), "static")
 MEDIA_ROOT = os.path.join(os.path.dirname(PROJECT_ROOT), "media")
+
+django_heroku.settings(locals())
