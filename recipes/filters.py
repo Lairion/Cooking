0c18a63d358d33e6fc9c8ff84f1720dfa8b0944b
manul <nunyes_m_a@ukr.net>
2018-08-23 17:47:51 +0300

    fix identations

--- recipes/filters.py
+++ recipes/filters.py
@@ -1,22 +1,22 @@
 import django_filters
 
 class RecipeFilter(django_filters.FilterSet):
-	count__gt = django_filters.NumberFilter(
-		field_name='price', 
-		lookup_expr='gt'
-		)
-	count__lt = django_filters.NumberFilter(
-		field_name='price',
-		lookup_expr='lt'
-		)
-	time__gt = django_filters.NumberFilter(
-		field_name='price', 
-		lookup_expr='gt'
-		)
-	time__lt = django_filters.NumberFilter(
-		field_name='price',
-		lookup_expr='lt'
-		)
+    count__gt = django_filters.NumberFilter(
+        field_name='price', 
+        lookup_expr='gt'
+        )
+    count__lt = django_filters.NumberFilter(
+        field_name='price',
+        lookup_expr='lt'
+        )
+    time__gt = django_filters.NumberFilter(
+        field_name='price', 
+        lookup_expr='gt'
+        )
+    time__lt = django_filters.NumberFilter(
+        field_name='price',
+        lookup_expr='lt'
+        )
     class Meta:
         model = Recipe
         fields = ['name','category_recipe','count','time']
