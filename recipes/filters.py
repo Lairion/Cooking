900e1eb52d8d98e50f4bf932dded11b120dfedca
manul <nunyes_m_a@ukr.net>
2018-08-23 17:55:56 +0300

    Change filter parameter

--- recipes/filters.py
+++ recipes/filters.py
@@ -3,21 +3,21 @@ from .models import Recipe
 
 class RecipeFilter(django_filters.FilterSet):
     count__gt = django_filters.NumberFilter(
-        field_name='price', 
+        field_name='count', 
         lookup_expr='gt'
         )
     count__lt = django_filters.NumberFilter(
-        field_name='price',
+        field_name='count',
         lookup_expr='lt'
         )
     time__gt = django_filters.NumberFilter(
-        field_name='price', 
+        field_name='time', 
         lookup_expr='gt'
         )
     time__lt = django_filters.NumberFilter(
-        field_name='price',
+        field_name='time',
         lookup_expr='lt'
         )
     class Meta:
         model = Recipe
-        fields = ['name','category_recipe','count','time']
+        fields = ['name','category_recipe']
